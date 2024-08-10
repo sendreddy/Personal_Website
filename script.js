6bd6c03ca60dbb2a501a9aed71aa93cd9f997474
Srujana <endreddy25@gmail.com>
2024-08-10 13:02:37 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+//targeting menu-links and hamburger-icon from html 
+
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+}
